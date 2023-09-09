f8ec4da50e530a279fc3072d60f9a7b36075e55a
ProSrm <srmate1516@gmail.com>
2023-09-09 23:16:57 +0530

    added data filling and axios api

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
+import { useState } from 'react';
 import './App.css';
 import TaskList from './Component/TaskList';
+import axios from 'axios'
 
 
 function App() {
 
-  const addTask = () => {
+  //storing values 
+  const [Task, setTasks] = useState();
+  const [Description, setDescription] = useState();
 
+  const addTask = () => {
+    axios.post("http://localhost:3001/addTask", { Task: Task, Desc: Description })
+      .then(result => console.log(result))
+      .catch(err => console.log(err));
     console.log("task added");
 
   }
@@ -15,8 +23,8 @@ function App() {
     <div className="App">
       <div className="App-header">
         <h4>Add your Tasks here</h4>
-        <input type="text" placeholder='Enter your Title here' />
-        <input type="text" placeholder='Enter your Description here' />
+        <input type="text" placeholder='Enter your Title here' onChange={(e) => setTasks(e.target.value)} />
+        <input type="text" placeholder='Enter your Description here' onChange={(e) => setDescription(e.target.value)} />
         <button onClick={addTask}>Add task </button>
       </div>
       <TaskList />
--- src/Component/TaskList.jsx
+++ src/Component/TaskList.jsx
@@ -1,31 +1,45 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import TaskCard from "./TaskCard";
 import "./css/TaskList.css";
-
+import axios from "axios"
 const TaskList = () => {
-
+    const [ToDO, setToDO] = useState([]);
+    //to fetch data . 
+    useEffect(() => {
+        axios.get("http://localhost:3001/getTasks")
+            .then(result => setToDO(result.data))
+            .catch(err => console.log(err))
+        console.log(ToDO);
+    }, [])
+    //function to drag and drop functionality
     const dragStarted = (e) => {
         e.preventDefault();
         console.log("drag started");
     }
-
+    //funciton to drag completion
     const drageComplete = (e) => {
         let taskid = e.dataTransfer.getData('taskid');
         console.log("You have Dropped an item");
     }
-
     return <div className="main">
         <div className="TOdo">
             <h5>Todo</h5>
+            {/* rendering taskcard component */}
+            {/* {
+                ToDO.length === 0 ? <div>Add item here</div> :
+                    ToDO.map(todo => (
+                        <TaskCard id={1} task={todo} />
+                    ))
+            } */}
             <TaskCard id={1} />
+
         </div>
         <div onDragOver={(e) => dragStarted(e)} onDrop={(e) => drageComplete(e)} className="Doing">
             <h5>Doing</h5>
 
         </div>
-        <div className="Done">
+        <div onDragOver={(e) => dragStarted(e)} onDrop={(e) => drageComplete(e)} className="Done">
             <h5>Done</h5>
-
         </div>
     </div>;
 };
